8309ff52292e0007ea02a5960f0842cac7b355c5
Ekaterina Lyamina <Ekaterina.Lyamina@ivkhk.ee>
2020-09-11 11:02:53 +0300

    Градусник

--- src/sptvr19_gradusforenheit/SPTVR19_GradusForenheit.java
+++ src/sptvr19_gradusforenheit/SPTVR19_GradusForenheit.java
@@ -16,6 +16,8 @@ public class SPTVR19_GradusForenheit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        App app = new App();
+        app.run();
     }
     
 }
